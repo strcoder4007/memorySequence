8b58edb75c7499de8da38e8b0b7b6b5a7fbb22c9
Shubham Singh <mynameshubham@gmail.com>
2017-07-22 01:04:32 +0400

    made it dark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,21 +10,19 @@ export class AppComponent implements OnInit{
     memories: Memory[];
 
     constructor() {
-        console.log("constructor working....");
     }
 
 
     ngOnInit() {
-        console.log("ngOnInit working....");
-        this.memory = {
+
+        var memory = {
             title: 'Inception', 
             content: 'The new syntax has a couple of things to note. The first is *ngFor. The * is a shorthand for using the new Angular template syntax with the template tag. This is also called a structural Directive. It is helpful to know that * is just a shorthand to explicitly defining the data bindings on a template tag. The template tag prevents the browser from reading or executing the code within it.'
         }
-        this.memories = [{title: 'inception', content:'whatever'}];
-        console.log(this.memories);
+        this.memories = [memory];
+
     }
-
-
+    
 }
 interface Memory {
     title: string;
