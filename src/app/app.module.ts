f2a26351c95ec4b0622c10aaf186d86dc9b70b0f
Shubham Singh <mynameshubham@gmail.com>
2017-07-25 20:15:08 +0400

    Option to add blogs (still open to all)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {Collapse} from './collapse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Collapse
   ],
   imports: [
-    BrowserModule
+      FormsModule,
+    BrowserModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
