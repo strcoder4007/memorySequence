0b8f87f02850ca9e60325bd2f95d7aec4e3192a9
Shubham Singh <mynameshubham@gmail.com>
2017-07-26 03:16:31 +0400

    added timestamp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,16 @@ export class AppComponent implements OnInit{
     memories =[];
     newTitle: string = "";
     newMemory: string = "";
+ 
+    /*
+   file: string = 'assets/data.json'
+   obj: any = {name: 'JP'}
+    
+   jsonfile.writeFile(file, obj, {flag: 'a'}, function (err) {
+     console.error(err)
+   })
+*/
+
 
     constructor(public http: Http) {
     }
@@ -32,10 +42,17 @@ export class AppComponent implements OnInit{
     }
 
     save() {
+        var today = new Date();
+        var dd = today.getDate();
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var mm = months[today.getMonth()];
+        var yyyy = today.getFullYear()
+        var mytime = dd + ' ' + mm + ' ' + yyyy;
         if(this.newMemory.length && this.newTitle.length){
             this.memory = {
                 title: this.newTitle,
-                content: this.newMemory
+                content: this.newMemory,
+                time: mytime
             }
             this.memories.unshift(this.memory);
         }
@@ -68,7 +85,8 @@ export class AppComponent implements OnInit{
         this.myImage = 'assets/img/yoda.png';
         this.memory = {
             title: `Inception`, 
-            content: `The new syntax has a couple of things to note. The first is *ngFor. The * is a shorthand for using the new Angular template syntax with the template tag. This is also called a structural Directive. It is helpful to know that * is just a shorthand to explicitly defining the data bindings on a template tag. The template tag prevents the browser from reading or executing the code within it. Shepard Fairey is one of those artists that you might not have heard of, but you've definitely seen at least one of his works. Unless you've been leaving under a rock for the past 10 years, you've seen the Obama Hope poster all around at some point. Well, that's Shepard right there. Shepard is a street art artist who began his career over 30 years ago, on his teen years. These days his been all over the world painting murals and designing beautiful posters and more. He's got the kind of art that will make you think whenever you see it. You might not share his political and world views, but he has definitely turn Social criticism into art. For more of his work, please visit his Instagram (for the latest) or his portfolio. I hope you enjoy these! Cheers. ;)`
+            content: `The new syntax has a couple of things to note. The first is *ngFor. The * is a shorthand for using the new Angular template syntax with the template tag. This is also called a structural Directive. It is helpful to know that * is just a shorthand to explicitly defining the data bindings on a template tag. The template tag prevents the browser from reading or executing the code within it. Shepard Fairey is one of those artists that you might not have heard of, but you've definitely seen at least one of his works. Unless you've been leaving under a rock for the past 10 years, you've seen the Obama Hope poster all around at some point. Well, that's Shepard right there. Shepard is a street art artist who began his career over 30 years ago, on his teen years. These days his been all over the world painting murals and designing beautiful posters and more. He's got the kind of art that will make you think whenever you see it. You might not share his political and world views, but he has definitely turn Social criticism into art. For more of his work, please visit his Instagram (for the latest) or his portfolio. I hope you enjoy these! Cheers. ;)`,
+            time: `20 July 2017`
         }
         this.memories.push(this.memory);
         this.getposts().subscribe((posts) => {
@@ -77,10 +95,13 @@ export class AppComponent implements OnInit{
                 this.memories.unshift(this.memory);
             }
         })
+
+
     }
 
 }
 interface Memory {
     title: string;
     content: any;
+    time: string;
 }
