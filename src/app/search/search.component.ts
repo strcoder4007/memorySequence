a630acefcd499d3c1cc094b9e0ebf7a7c2192e94
Shubham Singh <mynameshubham@gmail.com>
2017-07-27 01:00:31 +0400

    search working

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+    term: string  = '';
     items = [];
   constructor() { }
 
