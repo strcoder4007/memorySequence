de0138209d6dcd7c709f14ac73945883823ce8c1
Shubham Singh <mynameshubham@gmail.com>
2017-07-28 23:05:06 +0400

    sanitized html & integrated spotify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/map';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -21,9 +22,9 @@ export class AppComponent implements OnInit{
     showSearch: boolean = false;
     myTags = [];
     tags: Tags;
+    public html;
 
-
-    constructor(public http: Http) {
+    constructor(public http: Http, private sanitizer: DomSanitizer) {
 
     }
 
@@ -92,19 +93,14 @@ export class AppComponent implements OnInit{
         this.memColor = 'gray';
         this.seduce = "turn to light side";
         this.myImage = 'assets/img/yoda.png';
-        this.memory = {
-            title: `Inception`, 
-            content: `The new syntax has a couple of things to note. The first is *ngFor. The * is a shorthand for using the new Angular template syntax with the template tag. This is also called a structural Directive. It is helpful to know that * is just a shorthand to explicitly defining the data bindings on a template tag. The template tag prevents the browser from reading or executing the code within it. Shepard Fairey is one of those artists that you might not have heard of, but you've definitely seen at least one of his works. Unless you've been leaving under a rock for the past 10 years, you've seen the Obama Hope poster all around at some point. Well, that's Shepard right there. Shepard is a street art artist who began his career over 30 years ago, on his teen years. These days his been all over the world painting murals and designing beautiful posters and more. He's got the kind of art that will make you think whenever you see it. You might not share his political and world views, but he has definitely turn Social criticism into art. For more of his work, please visit his Instagram (for the latest) or his portfolio. I hope you enjoy these! Cheers. ;)`,
-            time: `20 July 2017`
-        }
-        this.memories.push(this.memory);
         this.getposts().subscribe((posts) => {
             for(var i = 0; i < posts.length; i++){
                 this.memory = posts[i];
                 this.tags = {
                     name: this.memory.title
                 }
-                this.myTags.push(this.tags.name);    
+                this.myTags.push(this.tags.name);
+                this.memory.content = this.sanitizer.bypassSecurityTrustHtml(this.memory.content);
                 this.memories.unshift(this.memory);
             }
         })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
+
 import { AppComponent } from './app.component';
 import {Collapse} from './collapse.component';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
@@ -22,6 +23,15 @@ import { SearchComponent } from './search/search.component';
       HttpModule,
       Ng2SearchPipeModule,
       RouterModule.forRoot([
+          /*
+        {
+            path: '',
+            redirectTo: '/home',
+            pathMatch: 'full'
+        },
+        {path: 'memories', component: MemoriesComponent},
+        {path: 'home', component: AppComponent}
+        */
       ])
   ],
   providers: [],
