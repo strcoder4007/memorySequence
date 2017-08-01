76d8ec908e66451bd7a09257cfc89363b4e9f3f0
Shubham Singh <mynameshubham@gmail.com>
2017-08-02 01:01:25 +0400

    bugs removed and added separate memory component

---
+++ src/app/memory/memory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-memory',
+  templateUrl: './memory.component.html',
+  styleUrls: ['./memory.component.css']
+})
+export class MemoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
