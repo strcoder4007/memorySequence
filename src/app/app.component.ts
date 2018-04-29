09055c57efc16a68efe48619185365c8d2721f97
Shubham Singh <mynameshubham@gmail.com>
2018-04-29 22:57:05 +0400

    hover word memory on memories page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,71 @@
-import { Component, OnInit, SecurityContext, Input, ViewChild, AfterViewInit } from '@angular/core';
-import { Http } from '@angular/http';
+import {
+    Component,
+    OnInit,
+    SecurityContext,
+    Input,
+    ViewChild,
+    AfterViewInit
+} from '@angular/core';
+import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { Router} from '@angular/router';
-import { MemoriesComponent } from './memories/memories.component';
-import { MemoryComponent } from './memory/memory.component';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import {Router} from '@angular/router';
+import {MemoriesComponent} from './memories/memories.component';
+import {MemoryComponent} from './memory/memory.component';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent implements OnInit, AfterViewInit{
-    myBgColor: string;
-    mySide: string;
-    seduce: string;
-    myColor: string;
-    memColor: string;
-    myImage: string;
-    showSearch: boolean = false;
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    isLoggedIn: boolean = false;
-    inLogin: boolean = false;
-    username: string;
-    password: string;
-    showError: boolean = false;
-    screenWidth: any;
-    inMemory: boolean = false;
-    hideOptions: boolean = true;
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
+export class AppComponent implements OnInit,
+AfterViewInit {
+    myBgColor : string;
+    mySide : string;
+    seduce : string;
+    myColor : string;
+    memColor : string;
+    myImage : string;
+    showSearch : boolean = false;
+    months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+    isLoggedIn : boolean = false;
+    inLogin : boolean = false;
+    username : string;
+    password : string;
+    showError : boolean = false;
+    screenWidth : any;
+    inMemory : boolean = false;
+    hideOptions : boolean = true;
     sortedMemories = [];
-    @ViewChild(MemoriesComponent) private memoriesComponent:MemoriesComponent;
-    @ViewChild(MemoryComponent) private memoryComponent:MemoryComponent;
+    @ViewChild(MemoriesComponent)private memoriesComponent : MemoriesComponent;
+    @ViewChild(MemoryComponent)private memoryComponent : MemoryComponent;
 
-    constructor(public http: Http, private sanitizer: DomSanitizer, private router: Router) {}
+    constructor(public http : Http, private sanitizer : DomSanitizer, private router : Router) {}
 
-    ngOnChanges() {
-
-    }
+    ngOnChanges() {}
 
     goBack() {
-        let curUrl = (window.location+'').split('/');
-        if(curUrl.length >= 5) {
-            this.router.navigate(['']);
+        let curUrl = (window.location + '').split('/');
+        if (curUrl.length >= 5) {
+            this
+                .router
+                .navigate(['']);
         }
     }
 
     gotoMemory(ev) {
-        this.router.navigate(['/memory', ev]);
+        this
+            .router
+            .navigate(['/memory', ev]);
         this.inMemory = true;
     }
 
@@ -59,33 +78,32 @@ export class AppComponent implements OnInit, AfterViewInit{
     }
 
     loggedIn(ev) {
-        if(!ev) {
+        if (!ev) {
             localStorage.setItem("loggedIn", "");
             localStorage.setItem("sortedMemoriesPrivate", "");
         }
         this.isLoggedIn = ev;
-        this.memoriesComponent.refreshData(ev);
+        this
+            .memoriesComponent
+            .refreshData(ev);
     }
 
     toggleMenu() {
-        if(this.showSearch)
+        if (this.showSearch) 
             this.showSearch = false;
-        else
+        else 
             this.showSearch = true;
-    }
-
+        }
+    
     toggleSide() {
-        if(this.mySide == 'light')
-        {
+        if (this.mySide == 'light') {
             this.mySide = 'dark';
             this.myBgColor = '#222222';
             this.seduce = "turn to light side";
             this.myColor = 'white';
             this.memColor = 'gray';
             this.myImage = 'assets/img/yoda.png';
-        }
-        else if(this.mySide == 'dark')
-        {
+        } else if (this.mySide == 'dark') {
             this.mySide = 'light';
             this.myBgColor = 'white';
             this.seduce = "turn to dark side";
@@ -102,22 +120,24 @@ export class AppComponent implements OnInit, AfterViewInit{
         this.memColor = 'gray';
         this.seduce = "turn to light side";
         this.myImage = 'assets/img/yoda.png';
-        if(localStorage.getItem("loggedIn") == undefined)
+        if (localStorage.getItem("loggedIn") == undefined) 
             localStorage.setItem("loggedIn", "");
-        else if(localStorage.getItem("loggedIn") == "xyufsvt") {
+        else if (localStorage.getItem("loggedIn") == "xyufsvt") {
             this.loggedIn(true);
         }
-        if(localStorage.getItem("lastModified") == undefined)
+        if (localStorage.getItem("lastModified") == undefined) 
             localStorage.setItem("lastModified", "");
-        if(localStorage.getItem("sortedMemoriesPrivate") == undefined)
+        if (localStorage.getItem("sortedMemoriesPrivate") == undefined) 
             localStorage.setItem("sortedMemoriesPrivate", "");
-        if(localStorage.getItem("sortedMemoriesPublic") == undefined)
+        if (localStorage.getItem("sortedMemoriesPublic") == undefined) 
             localStorage.setItem("sortedMemoriesPublic", "");
-    }
+        }
     ngAfterViewInit() {
-        let curUrl = (window.location+'').split('/');
-        if(curUrl.length >= 5) {
-            this.memoriesComponent.processJson();
+        let curUrl = (window.location + '').split('/');
+        if (curUrl.length >= 5) {
+            this
+                .memoriesComponent
+                .processJson();
             this.hideOptions = false;
         }
     }
