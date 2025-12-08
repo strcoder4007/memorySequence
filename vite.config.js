7979e75913ebeaa78b50df19923613c8771b1760
Shubham <mynameshubham23@gmail.com>
2025-12-08 17:53:48 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/memseq/',
+  base: '/memorySequence/',
   plugins: [vue()],
   server: {
     port: 5174,
